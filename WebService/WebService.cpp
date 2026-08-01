a367b94863303182eb67f6cd28705e13f2c440cd
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-01 19:27:24 +0800

    WebService: the SOAP maps and the request handler
    Read back out of .rdata; the provider that generated them cannot run here.

--- WebService/WebService.cpp
+++ WebService/WebService.cpp
@@ -1,11 +1,13 @@
 #define _WIN32_WINNT 0x0400
 #define _WIN32_DCOM
 
+#include "WebServiceMaps.h"
 #include "decomp.h"
 
 #include <atlbase.h>
 #include <atlcom.h>
 #include <atlisapi.h>
+#include <atlsoap.h>
 #include <boost/thread/once.hpp>
 
 static boost::once_flag flagInitRoblox = BOOST_ONCE_INIT;
@@ -23,6 +25,46 @@ class CDllMainOverride : public ATL::CAtlDllModuleT<CDllMainOverride>
 };
 
 CDllMainOverride _AtlModule;
+
+namespace Roblox {
+
+// SIZE 0x180
+class CWebService : public ATL::CSoapHandler<CWebService>
+{
+public:
+	// FUNCTION: WEBSERVICE 0x1000dcc0
+	const _soapmap** GetFunctionMap() { return ___Roblox_CWebService_funcs; }
+
+	// FUNCTION: WEBSERVICE 0x1000dcd0
+	const _soapmap** GetHeaderMap() { return ___Roblox_CWebService_headers; }
+
+	// FUNCTION: WEBSERVICE 0x1000dce0
+	void* GetHeaderValue() { return &errorMessage; }
+
+	// STUB: WEBSERVICE 0x1000dcf0
+	HRESULT CallFunction(void* pvParam, const wchar_t* wszLocalName, int cchLocalName, size_t nItem)
+	{
+		STUB(0x1000dcf0);
+		return E_NOTIMPL;
+	}
+
+	// FUNCTION: WEBSERVICE 0x1000df50
+	const wchar_t* GetNamespaceUri() { return L"urn:Roblox"; }
+
+	// FUNCTION: WEBSERVICE 0x1000df60
+	const char* GetNamespaceUriA() { return "urn:Roblox"; }
+
+	// FUNCTION: WEBSERVICE 0x1000df70
+	const char* GetServiceName() { return "Service"; }
+
+private:
+	undefined m_unk0x004[0x17c - 0x004]; // 0x004
+	ATL::CComBSTR errorMessage;          // 0x17c
+};
+
+} // namespace Roblox
+
+DECLARE_REQUEST_HANDLER("Default", CWebService, Roblox::CWebService)
 
 // FUNCTION: WEBSERVICE 0x1000e070
 STDAPI DllGetClassObject(REFCLSID clsid, REFIID iid, LPVOID* ppv)
---
+++ WebService/WebServiceMaps.h
@@ -0,0 +1,17 @@
+#ifndef WEBSERVICE_WEBSERVICEMAPS_H
+#define WEBSERVICE_WEBSERVICEMAPS_H
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0400
+#endif
+
+#include <atlsoap.h>
+
+namespace Roblox {
+
+extern const _soapmap* ___Roblox_CWebService_funcs[];
+extern const _soapmap* ___Roblox_CWebService_headers[];
+
+} // namespace Roblox
+
+#endif // WEBSERVICE_WEBSERVICEMAPS_H
