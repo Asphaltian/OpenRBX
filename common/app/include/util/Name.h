7340a99d2d5e05d11ce7b83362cb747176c7387a
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-02 13:40:45 +0800

    Bring up RBX::Name

---
+++ common/app/include/util/Name.h
@@ -0,0 +1,41 @@
+#ifndef UTIL_NAME_H
+#define UTIL_NAME_H
+
+#include <boost/noncopyable.hpp>
+#include <string>
+
+namespace RBX {
+
+// SIZE 0x20
+class Name : public boost::noncopyable
+{
+public:
+	static const Name& declare(const char* name, int dictionaryIndex);
+	static const Name& lookup(const char* name);
+	static const Name& lookup(const std::string& name);
+	static const Name& getNullName();
+
+	bool empty() const;
+
+	template <char** name>
+	static const Name& doDeclare()
+	{
+		static const Name& n = declare(*name, -1);
+
+		return n;
+	}
+
+	template <char** name>
+	static void callDoDeclare()
+	{
+		doDeclare<name>();
+	}
+
+private:
+	int dictionaryIndex; // 0x00
+	std::string name;    // 0x04
+};
+
+} // namespace RBX
+
+#endif // UTIL_NAME_H
---
+++ common/app/util/Name.cpp
@@ -0,0 +1,44 @@
+#include "util/Name.h"
+
+#include "decomp.h"
+
+namespace RBX {
+
+DECOMP_SIZE_ASSERT(Name, 0x20)
+
+// STUB: WEBSERVICE 0x10058840
+const Name& Name::declare(const char* name, int dictionaryIndex)
+{
+	STUB(0x10058840);
+	return getNullName();
+}
+
+// STUB: WEBSERVICE 0x10058a30
+const Name& Name::getNullName()
+{
+	STUB(0x10058a30);
+	return *(const Name*) NULL;
+}
+
+// STUB: WEBSERVICE 0x10058ad0
+const Name& Name::lookup(const std::string& name)
+{
+	STUB(0x10058ad0);
+	return getNullName();
+}
+
+// STUB: WEBSERVICE 0x10058ba0
+const Name& Name::lookup(const char* name)
+{
+	STUB(0x10058ba0);
+	return getNullName();
+}
+
+// STUB: WEBSERVICE 0x10058c70
+bool Name::empty() const
+{
+	STUB(0x10058c70);
+	return false;
+}
+
+} // namespace RBX
