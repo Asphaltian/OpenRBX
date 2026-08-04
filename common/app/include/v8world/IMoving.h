e4628986a8083cd99b0815a2d606ff7687afcbb2
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-04 14:49:36 +0800

    Match the v8world moving family

--- common/app/include/v8world/IMoving.h
+++ common/app/include/v8world/IMoving.h
@@ -3,24 +3,63 @@
 
 #include "decomp.h"
 
+#include <set>
+
 namespace RBX {
 
-class IMovingManager;
+class IMoving;
+
+// SIZE 0x18
+class __declspec(novtable) IMovingManager
+{
+public:
+	typedef std::set<IMoving*> MovingSet;
+
+	IMovingManager();
+
+	virtual ~IMovingManager(); // vtable+0x00
+
+	void onMovingHeartbeat();
+
+protected:
+	void moved(IMoving* m) { moving.insert(m); }
+
+	void remove(IMoving* m);
+
+private:
+	MovingSet moving;            // 0x04
+	MovingSet::iterator current; // 0x10
+
+	friend class IMoving;
+};
+
+DECOMP_SIZE_ASSERT(IMovingManager, 0x18)
 
 // SIZE 0x0c
 class __declspec(novtable) IMoving
 {
 protected:
-	virtual void onCanAggregateChanged() = 0; // vtable+0x00
+	virtual void onCanAggregateChanged(bool value) = 0; // vtable+0x00
 
 public:
+	IMoving();
+
 	virtual bool reportTouches() const = 0; // vtable+0x04
 
 	void notifyMoved();
 
+protected:
+	bool checkSleep();
+
+	void setMovingManager(IMovingManager* value);
+
 private:
+	void makeMoving();
+
 	IMovingManager* iMovingManager; // 0x04
 	int stepsToSleep;               // 0x08
+
+	friend class IMovingManager;
 };
 
 DECOMP_SIZE_ASSERT(IMoving, 0x0c)
--- common/app/v8world/IMoving.cpp
+++ common/app/v8world/IMoving.cpp
@@ -2,12 +2,96 @@
 
 #include "decomp.h"
 
+#include <cstddef>
+
 namespace RBX {
 
-// STUB: WEBSERVICE 0x100d0e40
+// FUNCTION: WEBSERVICE 0x100d0be0
+IMoving::IMoving() : iMovingManager(NULL), stepsToSleep(0)
+{
+}
+
+bool IMoving::checkSleep()
+{
+	if (stepsToSleep > 1) {
+		stepsToSleep--;
+
+		return false;
+	}
+
+	stepsToSleep = 0;
+	onCanAggregateChanged(true);
+
+	return true;
+}
+
+void IMoving::makeMoving()
+{
+	if (iMovingManager != NULL) {
+		iMovingManager->moved(this);
+	}
+}
+
+// FUNCTION: WEBSERVICE 0x100d0c80
+IMovingManager::~IMovingManager()
+{
+}
+
+// STUB: WEBSERVICE 0x100d0cc0
+void IMovingManager::remove(IMoving* m)
+{
+	if (current != moving.end() && *current == m) {
+		current = moving.erase(current);
+	}
+	else {
+		moving.erase(m);
+	}
+}
+
+// FUNCTION: WEBSERVICE 0x100d0d50
+void IMovingManager::onMovingHeartbeat()
+{
+	for (current = moving.begin(); current != moving.end();) {
+		IMoving* m = *current;
+
+		if (m->checkSleep()) {
+			remove(m);
+		}
+		else {
+			++current;
+		}
+	}
+}
+
+// FUNCTION: WEBSERVICE 0x100d0de0
+void IMoving::setMovingManager(IMovingManager* value)
+{
+	if (iMovingManager != NULL) {
+		iMovingManager->remove(this);
+	}
+
+	iMovingManager = value;
+
+	if (value != NULL && stepsToSleep > 0) {
+		onCanAggregateChanged(false);
+		makeMoving();
+	}
+}
+
+// FUNCTION: WEBSERVICE 0x100d0e40
 void IMoving::notifyMoved()
 {
-	STUB(0x100d0e40);
+	if (stepsToSleep == 0) {
+		stepsToSleep = 30;
+		onCanAggregateChanged(false);
+		makeMoving();
+	}
+}
+
+// FUNCTION: WEBSERVICE 0x100d0e80
+IMovingManager::IMovingManager()
+{
+	current = moving.end();
 }
 
 } // namespace RBX
