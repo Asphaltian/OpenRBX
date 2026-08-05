06e4e087cc55c5ec02efb25b880e333bfbf4cbd7
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-06 02:40:39 +0800

    Name the descriptor destructors and objects, promoting 23 initializers

--- common/app/include/v8datamodel/Feature.h
+++ common/app/include/v8datamodel/Feature.h
@@ -113,13 +113,13 @@ DECOMP_SIZE_ASSERT(MotorFeature, 0x118)
 // RBX::`dynamic initializer for 'prop_DesiredAngle''
 // STUB: WEBSERVICE 0x1021cd60
 // RBX::`dynamic initializer for 'prop_CurrentAngle''
-// STUB: WEBSERVICE 0x1021cda0
+// SYNTHETIC: WEBSERVICE 0x1021cda0
 // RBX::`dynamic initializer for 'prop_FaceId''
-// STUB: WEBSERVICE 0x1021cde0
+// SYNTHETIC: WEBSERVICE 0x1021cde0
 // RBX::`dynamic initializer for 'prop_TopBottom''
-// STUB: WEBSERVICE 0x1021ce20
+// SYNTHETIC: WEBSERVICE 0x1021ce20
 // RBX::`dynamic initializer for 'prop_LeftRight''
-// STUB: WEBSERVICE 0x1021ce60
+// SYNTHETIC: WEBSERVICE 0x1021ce60
 // RBX::`dynamic initializer for 'prop_InOut''
 // clang-format on
 
--- common/app/include/v8datamodel/Message.h
+++ common/app/include/v8datamodel/Message.h
@@ -42,7 +42,7 @@ DECOMP_SIZE_ASSERT(Hint, 0x124)
 // clang-format on
 
 // clang-format off
-// STUB: WEBSERVICE 0x1021d530
+// SYNTHETIC: WEBSERVICE 0x1021d530
 // RBX::`dynamic initializer for 'desc_Text''
 // clang-format on
 
