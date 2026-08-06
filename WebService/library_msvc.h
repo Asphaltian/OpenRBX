c99e0bf14b39f2a7a8bb369d81b159d706e32db9
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-06 08:05:54 +0800

    Claim the G3D statics and the synthetic destructors our build already emits

--- WebService/library_msvc.h
+++ WebService/library_msvc.h
@@ -1,12 +1,54 @@
 #ifdef 0
 
 // clang-format off
+// LIBRARY: WEBSERVICE 0x1012e300
+// G3D::_internal::postToClipboard
+// LIBRARY: WEBSERVICE 0x1012e380
+// G3D::_internal::createErrorMessage
+// LIBRARY: WEBSERVICE 0x10134220
+// G3D::checkForCPUID
+// LIBRARY: WEBSERVICE 0x10134260
+// G3D::getStandardProcessorExtensions
+// LIBRARY: WEBSERVICE 0x101343b0
+// G3D::initTime
+// LIBRARY: WEBSERVICE 0x10139a40
+// G3D::findRayCapsuleIntersectionAux
+// LIBRARY: WEBSERVICE 0x1013a6c0
+// G3D::findRayCapsuleIntersection
+// LIBRARY: WEBSERVICE 0x1013ae10
+// G3D::getKeyFromString
+// LIBRARY: WEBSERVICE 0x1013b6f0
+// G3D::textPrompt
+// LIBRARY: WEBSERVICE 0x1013bb20
+// G3D::guiPrompt
+// LIBRARY: WEBSERVICE 0x1013bd60
+// G3D::needSwapBytes
+// LIBRARY: WEBSERVICE 0x1013bd80
+// G3D::readUInt32
+// LIBRARY: WEBSERVICE 0x1014aeb0
+// G3D::tokenTypeToString
 // LIBRARY: WEBSERVICE 0x101e82fc
 // pre_c_init
 // LIBRARY: WEBSERVICE 0x101e850c
 // __DllMainCRTStartup
 // LIBRARY: WEBSERVICE 0x101e9008
 // has_osfxsr_set
+// LIBRARY: WEBSERVICE 0x1021e950
+// `dynamic initializer for 'G3D::Matrix4::IDENTITY''
+// LIBRARY: WEBSERVICE 0x1021e9b0
+// `dynamic initializer for 'G3D::Matrix4::ZERO''
+// LIBRARY: WEBSERVICE 0x1021ea00
+// `dynamic initializer for 'G3D::Matrix3::ZERO''
+// LIBRARY: WEBSERVICE 0x1021ea40
+// `dynamic initializer for 'G3D::Matrix3::IDENTITY''
+// LIBRARY: WEBSERVICE 0x1021ea80
+// `dynamic initializer for 'G3D::Vector3::INF3''
+// LIBRARY: WEBSERVICE 0x1021eaf0
+// `dynamic initializer for 'G3D::Vector3::NAN3''
+// LIBRARY: WEBSERVICE 0x1021eb60
+// `dynamic initializer for 'G3D::CollisionDetection::ignoreArray''
+// LIBRARY: WEBSERVICE 0x1021eb70
+// G3D::_internal::`dynamic initializer for 'currentFilesUsed''
 // LIBRARY: WEBSERVICE 0x10220e3c
 // ATL::`dynamic initializer for '_AtlBaseModule''
 // LIBRARY: WEBSERVICE 0x10220e52
@@ -17,6 +59,14 @@
 // ATL::`dynamic initializer for 'g_strmgr''
 // LIBRARY: WEBSERVICE 0x10220eab
 // ATL::`dynamic initializer for '_AtlComModule''
+// LIBRARY: WEBSERVICE 0x102256f0
+// `G3D::System::operatingSystem'::`2'::`dynamic atexit destructor for '_operatingSystem''
+// LIBRARY: WEBSERVICE 0x10225700
+// `G3D::System::cpuArchitecture'::`2'::`dynamic atexit destructor for '_cpuArch''
+// LIBRARY: WEBSERVICE 0x10225710
+// `dynamic atexit destructor for 'G3D::CollisionDetection::ignoreArray''
+// LIBRARY: WEBSERVICE 0x10225730
+// G3D::_internal::`dynamic atexit destructor for 'currentFilesUsed''
 // LIBRARY: WEBSERVICE 0x10226437
 // ATL::`dynamic atexit destructor for '_AtlBaseModule''
 // LIBRARY: WEBSERVICE 0x10226441
