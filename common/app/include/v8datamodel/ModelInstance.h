48bf7805857a8c7413f38c56f692e4d6967d9d42
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-07 14:58:25 +0800

    Recover ModelInstance and its ComputeProp members

--- common/app/include/v8datamodel/ModelInstance.h
+++ common/app/include/v8datamodel/ModelInstance.h
@@ -2,19 +2,88 @@
 #define V8DATAMODEL_MODELINSTANCE_H
 
 #include "decomp.h"
+#include "util/ComputeProp.h"
+#include "util/Extents.h"
+#include "util/ICameraSubject.h"
+#include "util/IRenderable.h"
+#include "util/ISelectable3d.h"
+#include "v8datamodel/IPrimaryPart.h"
+#include "v8datamodel/PVInstance.h"
+
+#include <G3D/CoordinateFrame.h>
+#include <boost/shared_ptr.hpp>
 
 namespace RBX {
+
+class PartInstance;
 
 extern char sModel[];
 
 // SIZE 0x254
-class ModelInstance
+class ModelInstance : public PVInstance,
+					  public IRenderable,
+					  public virtual IPrimaryPart,
+					  public virtual ICameraSubject,
+					  public virtual ISelectable3d
 {
 public:
 	static bool showModelCoord;
 
 private:
-	undefined m_unk0x000[0x254 - 0x000]; // 0x000
+	CoordinateFrame modelInPrimary;                       // 0x178
+	PartInstance* primaryPart;                            // 0x1a8
+	boost::shared_ptr<PartInstance> candidatePrimaryPart; // 0x1ac
+
+	void updatePrimaryPart();
+
+	ComputeProp<float, ModelInstance> FlagHeight;         // 0x1b8
+	ComputeProp<Extents, ModelInstance> LocalGridExtents; // 0x1d8
+	ComputeProp<Extents, ModelInstance> WorldGridExtents; // 0x208
+
+	float computeFlagHeight() const;
+	Extents computeLocalGridExtents() const;
+	Extents computeWorldGridExtents() const;
+	void dirtyAll();
+
+	virtual bool shouldRender3dAdorn() const;
+	virtual void render3dAdorn(Adorn* adorn);
+	virtual void render3dSelect(Adorn* adorn, SelectState selectState);
+
+protected:
+	virtual void onDescendentAdded(Instance* instance);
+	virtual void onDescendentRemoving(const shared_ptr<Instance>& instance);
+	virtual bool askSetParent(const Instance* instance) const;
+
+private:
+	virtual void onLastChildRemoved();
+
+public:
+	ModelInstance();
+	virtual ~ModelInstance();
+
+	PartInstance* getPrimaryPartInternal();
+	void setPrimaryPart(PartInstance* value);
+
+	const CoordinateFrame& getModelInPrimary() const;
+	void setModelInPrimary(const CoordinateFrame& value);
+
+	virtual void onExtentsChanged() const;
+
+	virtual const Primitive* getBiggestPrimitive() const;
+	virtual bool hitTest(const G3D::Ray& ray, G3D::Vector3& hitPoint);
+
+	virtual Extents getExtentsWorld() const;
+	virtual Extents getExtentsLocal() const;
+
+	virtual const CoordinateFrame getLocation() const;
+
+	virtual void onCameraNear(float distance);
+	virtual void getCameraIgnorePrimitives(std::vector<const Primitive*>& primitives);
+
+	virtual PartInstance* getPrimaryPart();
+	virtual const PartInstance* getPrimaryPartConst() const;
+
+	virtual void legacyTraverseState(const CoordinateFrame& state);
 };
 
 DECOMP_SIZE_ASSERT(ModelInstance, 0x254)
--- common/app/v8datamodel/ModelInstance.cpp
+++ common/app/v8datamodel/ModelInstance.cpp
@@ -10,6 +10,162 @@ char sModel[] = "Model";
 
 bool ModelInstance::showModelCoord;
 
+bool ModelInstance::askSetParent(const Instance* instance) const
+{
+	STUB(0x10059c10);
+	return false;
+}
+
+// STUB: WEBSERVICE 0x10059c40
+float ModelInstance::computeFlagHeight() const
+{
+	STUB(0x10059c40);
+	return 0;
+}
+
+// STUB: WEBSERVICE 0x1005a1a0
+void ModelInstance::onExtentsChanged() const
+{
+	STUB(0x1005a1a0);
+}
+
+// STUB: WEBSERVICE 0x1005a1c0
+const Primitive* ModelInstance::getBiggestPrimitive() const
+{
+	STUB(0x1005a1c0);
+	return NULL;
+}
+
+// STUB: WEBSERVICE 0x1005a1f0
+void ModelInstance::updatePrimaryPart()
+{
+	STUB(0x1005a1f0);
+}
+
+// STUB: WEBSERVICE 0x1005a350
+Extents ModelInstance::computeWorldGridExtents() const
+{
+	STUB(0x1005a350);
+	return Extents();
+}
+
+// STUB: WEBSERVICE 0x1005a3b0
+const CoordinateFrame ModelInstance::getLocation() const
+{
+	STUB(0x1005a3b0);
+	return CoordinateFrame();
+}
+
+// STUB: WEBSERVICE 0x1005a460
+void ModelInstance::render3dSelect(Adorn* adorn, SelectState selectState)
+{
+	STUB(0x1005a460);
+}
+
+// STUB: WEBSERVICE 0x1005a890
+ModelInstance::~ModelInstance()
+{
+	STUB(0x1005a890);
+}
+
+// STUB: WEBSERVICE 0x1005ab00
+Extents ModelInstance::getExtentsWorld() const
+{
+	STUB(0x1005ab00);
+	return Extents();
+}
+
+// STUB: WEBSERVICE 0x1005ab50
+Extents ModelInstance::getExtentsLocal() const
+{
+	STUB(0x1005ab50);
+	return Extents();
+}
+
+// STUB: WEBSERVICE 0x1005ac90
+void ModelInstance::legacyTraverseState(const CoordinateFrame& state)
+{
+	STUB(0x1005ac90);
+}
+
+// STUB: WEBSERVICE 0x1005ad40
+bool ModelInstance::hitTest(const G3D::Ray& ray, G3D::Vector3& hitPoint)
+{
+	STUB(0x1005ad40);
+	return false;
+}
+
+// STUB: WEBSERVICE 0x1005add0
+bool ModelInstance::shouldRender3dAdorn() const
+{
+	STUB(0x1005add0);
+	return false;
+}
+
+// STUB: WEBSERVICE 0x1005ae20
+void ModelInstance::render3dAdorn(Adorn* adorn)
+{
+	STUB(0x1005ae20);
+}
+
+// STUB: WEBSERVICE 0x1005ae80
+void ModelInstance::onCameraNear(float distance)
+{
+	STUB(0x1005ae80);
+}
+
+// STUB: WEBSERVICE 0x1005b220
+PartInstance* ModelInstance::getPrimaryPart()
+{
+	STUB(0x1005b220);
+	return NULL;
+}
+
+const PartInstance* ModelInstance::getPrimaryPartConst() const
+{
+	STUB(0x1005b221);
+	return NULL;
+}
+
+// STUB: WEBSERVICE 0x1005b230
+void ModelInstance::onDescendentAdded(Instance* instance)
+{
+	STUB(0x1005b230);
+}
+
+// STUB: WEBSERVICE 0x1005b2c0
+void ModelInstance::onDescendentRemoving(const shared_ptr<Instance>& instance)
+{
+	STUB(0x1005b2c0);
+}
+
+// STUB: WEBSERVICE 0x1005b440
+void ModelInstance::getCameraIgnorePrimitives(std::vector<const Primitive*>& primitives)
+{
+	STUB(0x1005b440);
+}
+
+// STUB: WEBSERVICE 0x1005b560
+Extents ModelInstance::computeLocalGridExtents() const
+{
+	STUB(0x1005b560);
+	return Extents();
+}
+
+// STUB: WEBSERVICE 0x1005c000
+ModelInstance::ModelInstance()
+	: PVInstance(sModel), primaryPart(NULL), FlagHeight(this, &ModelInstance::computeFlagHeight),
+	  LocalGridExtents(this, &ModelInstance::computeLocalGridExtents),
+	  WorldGridExtents(this, &ModelInstance::computeWorldGridExtents)
+{
+	STUB(0x1005c000);
+}
+
+void ModelInstance::onLastChildRemoved()
+{
+	STUB(0x1015b611);
+}
+
 } // namespace RBX
 
 template const RBX::Name& RBX::Name::doDeclare<RBX::sPart>();
