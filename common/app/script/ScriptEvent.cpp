07cb5b2fc2f72a9c9f8425e207e9bf17ef823a5c
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-09 15:01:34 +0800

    Recover YieldingThreads::queueWaiter and carve the WaitingThread constructor

--- common/app/script/ScriptEvent.cpp
+++ common/app/script/ScriptEvent.cpp
@@ -1,11 +1,31 @@
 #include "script/ScriptEvent.h"
 
+#include <lua.h>
+
 namespace RBX {
 namespace Lua {
+
+// STUB: WEBSERVICE 0x100b5790
+YieldingThreads::WaitingThread::WaitingThread(lua_State* L, double requestedDelay)
+	: thread(new ThreadRef(L)), requestedDelay(requestedDelay), elapsedTime(0.0)
+{
+}
 
 // FUNCTION: WEBSERVICE 0x100b5e00
 YieldingThreads::YieldingThreads(ScriptContext* context) : context(context)
 {
+}
+
+// STUB: WEBSERVICE 0x100b5f80
+void YieldingThreads::queueWaiter(lua_State* L, double requestedDelay)
+{
+	waitingThreads.push_back(WaitingThread(L, requestedDelay));
+}
+
+// FUNCTION: WEBSERVICE 0x100b61d0
+void YieldingThreads::queueWaiter(lua_State* L)
+{
+	queueWaiter(L, 0.0);
 }
 
 } // namespace Lua
