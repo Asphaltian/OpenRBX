431aae9e582ac62947237ebaff10442b7daef559
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-10 14:05:19 +0800

    Recover the ServiceProvider descendent and child-add hooks

--- common/app/v8tree/Service.cpp
+++ common/app/v8tree/Service.cpp
@@ -4,12 +4,32 @@ namespace RBX {
 
 const char sServiceProvider[] = "ServiceProvider";
 
+// FUNCTION: WEBSERVICE 0x10058c30
+void ServiceProvider::onDescendentAdded(Instance* instance)
+{
+	Instance::onDescendentAdded(instance);
+	instance->onServiceProvider(NULL, this);
+}
+
+// FUNCTION: WEBSERVICE 0x10058c90
+void ServiceProvider::onDescendentRemoving(const shared_ptr<Instance>& instance)
+{
+	instance->onServiceProvider(this, NULL);
+	Instance::onDescendentRemoving(instance);
+}
+
 // STUB: WEBSERVICE 0x10059270
 Instance* ServiceProvider::findServiceByClassName(const Name& className) const
 {
 	STUB(0x10059270);
 
 	return NULL;
+}
+
+// FUNCTION: WEBSERVICE 0x1007ab90
+bool ServiceProvider::askAddChild(const Instance* instance) const
+{
+	return dynamic_cast<const Service*>(instance) != NULL;
 }
 
 } // namespace RBX
