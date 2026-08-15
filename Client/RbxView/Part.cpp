0c9dc1461bbe880da25a242c68575a9cbb0e8ebb
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2026-08-15 21:56:58 +0800

    Recover PartChunk and open the RbxView library

---
+++ Client/RbxView/Part.cpp
@@ -0,0 +1,93 @@
+#include "Part.h"
+
+#include "v8datamodel/PartInstance.h"
+
+namespace RBX {
+namespace View {
+
+// FUNCTION: WEBSERVICE 0x100e6150 FOLDED
+bool PartChunk::castsShadows() const
+{
+	return true;
+}
+
+// FUNCTION: WEBSERVICE 0x100e6150 FOLDED
+bool PartChunk::cullable() const
+{
+	return true;
+}
+
+// FUNCTION: WEBSERVICE 0x1016c740
+const G3D::CoordinateFrame& PartChunk::cframe()
+{
+	coordinateFrame = partInstance->getCoordinateFrame();
+
+	return coordinateFrame;
+}
+
+// STUB: WEBSERVICE 0x1016d100
+void PartChunk::onAncestorChanged(boost::shared_ptr<Instance>)
+{
+	STUB(0x1016d100);
+}
+
+// STUB: WEBSERVICE 0x1016d320
+void PartChunk::invalidateMesh()
+{
+	STUB(0x1016d320);
+}
+
+// STUB: WEBSERVICE 0x1016d410
+void PartChunk::invalidateMaterial()
+{
+	STUB(0x1016d410);
+}
+
+// STUB: WEBSERVICE 0x1016d500
+void PartChunk::onSpecialShapeChanged()
+{
+	STUB(0x1016d500);
+}
+
+// STUB: WEBSERVICE 0x1016d510
+void PartChunk::onPropertyChanged(const Reflection::PropertyDescriptor*)
+{
+	STUB(0x1016d510);
+}
+
+// FUNCTION: WEBSERVICE 0x1016d5d0
+G3D::ReferenceCountedPointer<Render::Mesh> PartChunk::getMesh()
+{
+	if (mesh.isNull()) {
+		updateMesh();
+	}
+
+	return mesh;
+}
+
+// STUB: WEBSERVICE 0x1016e0e0
+void PartChunk::onChildRemoved(boost::shared_ptr<Instance>)
+{
+	STUB(0x1016e0e0);
+}
+
+// STUB: WEBSERVICE 0x1016f210
+PartChunk::~PartChunk()
+{
+}
+
+// STUB: WEBSERVICE 0x10173c90
+void PartChunk::onChildAdded(boost::shared_ptr<Instance>)
+{
+	STUB(0x10173c90);
+}
+
+// STUB: WEBSERVICE 0x10174160
+PartChunk::PartChunk(float polygonOffset, const boost::shared_ptr<PartInstance>& partInstance, View* view)
+	: Render::Chunk(polygonOffset), partInstance(partInstance), materialInvalid(true), view(view), specialShape(NULL)
+{
+	STUB(0x10174160);
+}
+
+} // namespace View
+} // namespace RBX
---
+++ Client/RbxView/Part.h
@@ -0,0 +1,78 @@
+#ifndef RBXVIEW_PART_H
+#define RBXVIEW_PART_H
+
+#include "decomp.h"
+#include "renderlib/Chunk.h"
+#include "renderlib/Material.h"
+#include "renderlib/Mesh.h"
+
+#include <G3D/CoordinateFrame.h>
+#include <G3D/ReferenceCount.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/signals/connection.hpp>
+
+namespace RBX {
+
+class Instance;
+class PartInstance;
+class SpecialShape;
+
+namespace Reflection {
+class PropertyDescriptor;
+}
+
+namespace View {
+
+class View;
+
+// SIZE 0xcc
+class __declspec(novtable) PartChunk : public Render::Chunk
+{
+private:
+	G3D::CoordinateFrame coordinateFrame; // 0x1c
+
+	virtual void updateMesh() = 0; // vtable+0x20
+
+	boost::signals::scoped_connection childAddedConnection;           // 0x4c
+	boost::signals::scoped_connection childRemovedConnection;         // 0x60
+	boost::signals::scoped_connection ancestorChangedConnection;      // 0x74
+	boost::signals::scoped_connection propertyChangedConnection;      // 0x88
+	boost::signals::scoped_connection shapePropertyChangedConnection; // 0x9c
+
+public:
+	virtual ~PartChunk();
+
+	virtual bool castsShadows() const;
+	virtual bool cullable() const;
+
+protected:
+	boost::shared_ptr<PartInstance> partInstance;            // 0xb0
+	G3D::ReferenceCountedPointer<Render::Material> material; // 0xb8
+	bool materialInvalid;                                    // 0xbc
+	G3D::ReferenceCountedPointer<Render::Mesh> mesh;         // 0xc0
+	View* view;                                              // 0xc4
+	SpecialShape* specialShape;                              // 0xc8
+
+	PartChunk(float polygonOffset, const boost::shared_ptr<PartInstance>& partInstance, View* view);
+
+	void invalidateMaterial();
+	void invalidateMesh();
+
+	virtual void onPropertyChanged(const Reflection::PropertyDescriptor* descriptor); // vtable+0x24
+
+	virtual G3D::ReferenceCountedPointer<Render::Mesh> getMesh();
+	virtual const G3D::CoordinateFrame& cframe();
+
+private:
+	void onAncestorChanged(boost::shared_ptr<Instance> ancestor);
+	void onChildAdded(boost::shared_ptr<Instance>);
+	void onChildRemoved(boost::shared_ptr<Instance>);
+	void onSpecialShapeChanged();
+};
+
+DECOMP_SIZE_ASSERT(PartChunk, 0xcc)
+
+} // namespace View
+} // namespace RBX
+
+#endif // RBXVIEW_PART_H
